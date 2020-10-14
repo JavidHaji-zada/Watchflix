e9679f1e08d3c471abfba6eb09f9df6f0ff17803
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2020-10-14 21:13:01 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Navigation from './components/navigation';
+import Header from './components/header';
+import Features from './components/features';
+import About from './components/about';
+import Services from './components/services';
+import Gallery from './components/gallery';
+import Testimonials from './components/testimonials';
+import Team from './components/team';
+import Contact from './components/contact';
+import JsonData from './data/data.json';
+
+export class App extends Component {
+  state = {
+    landingPageData: {},
+  }
+  getlandingPageData() {
+    this.setState({landingPageData : JsonData})
+  }
+
+  componentDidMount() {
+    this.getlandingPageData();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Header data={this.state.landingPageData.Header} />
+        <Features data={this.state.landingPageData.Features} />
+        <About data={this.state.landingPageData.About} />
+        <Team data={this.state.landingPageData.Team} />
+      </div>
+    )
+  }
+}
+
+export default App;
