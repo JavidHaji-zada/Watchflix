cd83e7ce6a69c93e88cc861f6f0dea8ee65ecc2d
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2020-10-14 21:16:50 +0300

    Updates

---
+++ precache-manifest.9df0af98187383198e42d4605f5c3cfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b863528845fb097513c1169e0d9c5b",
+    "url": "/Watchflix/index.html"
+  },
+  {
+    "revision": "81250bb7058643ccdae9",
+    "url": "/Watchflix/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "795d54778189cfda167c",
+    "url": "/Watchflix/static/js/2.45f6f542.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Watchflix/static/js/2.45f6f542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81250bb7058643ccdae9",
+    "url": "/Watchflix/static/js/main.85f498bf.chunk.js"
+  },
+  {
+    "revision": "558956eaf080696f26b5",
+    "url": "/Watchflix/static/js/runtime-main.349f671f.js"
+  }
+]);
