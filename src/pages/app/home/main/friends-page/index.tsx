8647b65235710685a3633e773a462bb5e6f4ef43
leylaIsmayilova <leyla.ismayilova@ug.bilkent.edu.tr>
2020-12-26 03:56:13 +0300

    friends list is edited

--- src/pages/app/home/main/friends-page/index.tsx
+++ src/pages/app/home/main/friends-page/index.tsx
@@ -1,23 +1,86 @@
-import React, { HTMLAttributes, useState } from "react";
+import { wrap } from "module";
+import React, { HTMLAttributes, useEffect, useState } from "react";
 import { Button, ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap";
+import { FaUserAlt } from 'react-icons/fa';
+import { BsChevronDoubleRight } from 'react-icons/bs';
+import { User, UserType } from "../../../../../shared/models/user";
 
 //import { APP_STYLES } from "../../../../shared/styles";
-
 function Friends(): JSX.Element {
-    return(
-        <div>
-            <div className="row" style={{ color: "#808080" }}>
-                <div className="col py-3 px-lg-5 border bg-light"> Lily Mean Aldrin</div>
-                <div className="col py-3 px-lg-5 border bg-light">Marshal Erikson</div>
-                <div className="w-100"></div>
-                <div className="col py-3 px-lg-5 border bg-light">Ted Loser Mosby</div>
-                <div className="col py-3 px-lg-5 border bg-light">Barney Kasar Stinson</div>
-                <div className="w-100"></div>
-                <div className="col py-3 px-lg-5 border bg-light">Robin Scherbatsky</div>
-                <div className="col py-3 px-lg-5 border bg-light">Blitz</div>
-            </div>
-        </div>
-    )
+  const [friends, setFriends] = useState<User[]>([]);
+
+  useEffect(() => {
+    let newUser = new User({
+      username: "Leylusa",
+      fullname: "Leyla Hashimli",
+      email: "leyla.hashimli@lolu.com",
+      birthday: new Date("09/12/2000"),
+    });
+    let userList = [];
+    userList.push(newUser);
+    newUser = new User({
+      username: "Leylusa",
+      fullname: "Javid Hashimli",
+      email: "leyla.hashimli@lolu.com",
+      birthday: new Date("09/12/2000"),
+    });
+    userList.push(newUser);
+
+    newUser = new User({
+      username: "Leylusa",
+      fullname: "A Huseynov",
+      email: "leyla.hashimli@lolu.com",
+      birthday: new Date("09/12/2000"),
+    });
+    userList.push(newUser);
+    newUser = new User({
+      username: "Leylusa",
+      fullname: "Leyla Ismayilova",
+      email: "leyla.hashimli@lolu.com",
+      birthday: new Date("09/12/2000"),
+    });
+    userList.push(newUser);
+    setFriends(userList);
+  });
+
+  return (
+    <div style={{ flex: 1, fontSize: 24, color: "white" }}>
+      <BsChevronDoubleRight />  Friends
+      <div
+        style={{
+          flexWrap: "wrap",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        {friends.map((user) => (
+          <div
+            style={{...styles.friends }}
+          >
+            <FaUserAlt  />
+            <p style={{ fontSize: "24px", color: "white", marginLeft: 8 }}>   {user.fullname}</p> 
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+const styles = {
+  container: {
+    display: "flex",
+  },
+  friends: {
+    alignSelf: "baseline",
+    backgroundColor:'#333333',
+    color: "red",
+    width: "33%",
+    margin: "2.5%",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    boxShadow: "rgba(0, 0, 0) 0px 5px 15px",
+  },
+};
 
 export default Friends;
