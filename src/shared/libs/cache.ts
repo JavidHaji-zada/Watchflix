ddd249a307f99343f4f2eb38ba17dc01927e2de5
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2020-12-28 15:49:59 +0300

    login error case handled

--- src/shared/libs/cache.ts
+++ src/shared/libs/cache.ts
@@ -1,3 +1,12 @@
+import { CompanyUser, User } from "../models/user";
+
 export class Cache {
-    
+
+    static setCurrenUser(user: User | CompanyUser): void {
+        localStorage.setItem('currentUser', JSON.stringify(user))
+    }
+
+    static logoutUser(): void {
+        localStorage.removeItem('currentUser');
+    }
 }
